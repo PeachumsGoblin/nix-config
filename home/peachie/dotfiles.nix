38e5851a44e7cc2127dd1514abce10645b12bd0d
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-06 23:34:56 -0700

    Kitty customization

--- home/peachie/dotfiles.nix
+++ home/peachie/dotfiles.nix
@@ -1,25 +1,3 @@
 { config, pkgs, theme, ... }:
 
-{
-  home.file.".config/kitty/kitty.conf".text = ''
-    foreground #${theme.base05}
-    background #${theme.base00}
-    selection_background #${theme.base02}
-    color0 #${theme.base00}
-    color1 #${theme.base08}
-    color2 #${theme.base0B}
-    color3 #${theme.base0A}
-    color4 #${theme.base0D}
-    color5 #${theme.base0E}
-    color6 #${theme.base0C}
-    color7 #${theme.base05}
-    color8 #${theme.base03}
-    color9 #${theme.base08}
-    color10 #${theme.base0B}
-    color11 #${theme.base0A}
-    color12 #${theme.base0D}
-    color13 #${theme.base0E}
-    color14 #${theme.base0C}
-    color15 #${theme.base07}
-  '';
-}
+{}
