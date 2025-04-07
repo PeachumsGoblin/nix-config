2ef861fa4df49f8c30fd3ab94403d3fcd8b8c65b
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-06 19:40:19 -0700

    Added theme base16 files

---
+++ themes/catppuccin-latte.nix
@@ -0,0 +1,19 @@
+{
+  name = "Catppuccin Latte";
+  base00 = "#eff1f5"; # base, Default Background
+  base01 = "#e6e9ef"; # mantle, Lighter Background
+  base02 = "#ccd0da"; # surface0, Selection Background
+  base03 = "#bcc0cc"; # surface1, Comments, Invisibles
+  base04 = "#acb0be"; # surface2, Dark Foreground (Used for status bars)
+  base05 = "#4c4f69"; # text, Default Foreground, Caret, Delimiters, Operators
+  base06 = "#dc8a78"; # rosewater, Light Foreground (Not often used)
+  base07 = "#7287fd"; # lavender, Light Background (Not often used)
+  base08 = "#d20f39"; # red, Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+  base09 = "#fe640b"; # peach, Integers, Boolean, Constants, XML Attributes, Markup Link Url
+  base0A = "#df8e1d"; # yellow, Classes, Markup Bold, Search Text Background
+  base0B = "#40a02b"; # green, Strings, Inherited Class, Markup Code, Diff Inserted
+  base0C = "#179299"; # teal, Support, Regular Expressions, Escape Characters, Markup Quotes
+  base0D = "#1e66f5"; # blue, Functions, Methods, Attribute IDs, Headings
+  base0E = "#8839ef"; # mauve, Keywords, Storage, Selector, Markup Italic, Diff Changed
+  base0F = "#dd7878"; # flamingo, Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
+}
