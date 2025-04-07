1daafac9a7a91faf7a15bc3be23c925fc8f71287
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-06 23:43:12 -0700

    ZSH aliases

--- home/peachie/shell.nix
+++ home/peachie/shell.nix
@@ -11,17 +11,28 @@
       theme = "robbyrussell";
     };
     shellAliases = {
+      # System
+      _ = "sudo";
+      clr = "clear";
       ll = "ls -la";
+      snrs = "sudo nixos-rebuild switch --flake ~/nix-config#peach-koopy";
+      # Git
+      ga = "git add";
+      gaa = "git add all";
+      gcam = "git commit --all --message";
+      gcl = "git clone";
+      gp = "git push";
+      gpm = "git push -u origin main";
       gs = "git status";
+      # Themes
       toggle-theme = "$HOME/nix-config/scripts/toggle-theme.sh";
       theme = "$HOME/nix-config/scripts/switch-theme.sh";
       dark-theme = "$HOME/nix-config/scripts/set-dark-theme.sh";
       light-theme = "$HOME/nix-config/scripts/set-light-theme.sh";
     };
-#    initExtra = ''
-#      export EDITOR=nano
-#      eval "$(zoxide init zsh)"
-#    '';
+    initExtra = ''
+      export EDITOR=nano
+    '';
   };
 
   programs.starship = {
