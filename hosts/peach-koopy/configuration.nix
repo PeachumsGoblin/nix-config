1fe96de8a0942744bc94a06ce15a5eb6e63c8327
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-06 22:03:29 -0700

    Adding systemd services

--- hosts/peach-koopy/configuration.nix
+++ hosts/peach-koopy/configuration.nix
@@ -9,6 +9,7 @@
     ../../modules/system/users.nix
     ../../modules/system/fonts.nix
     ../../modules/system/home-manager-cli.nix
+    ../../modules/system/packages.nix
     ../../modules/games/steam-and-launchers.nix
     ../../modules/games/bolt-launcher.nix
   ];
