af5759d9019aaf91fdb99ca3f07bd282fbcf7857
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-06 22:07:24 -0700

    Adding systemd services

--- home/peachie/shell.nix
+++ home/peachie/shell.nix
@@ -13,8 +13,8 @@
     shellAliases = {
       ll = "ls -la";
       gs = "git status";
-      toggle-theme = "~/nix-config/scripts/toggle-theme.sh";
-      theme = "~/nix-config/scripts/switch-theme.sh";
+      toggle-theme = "$HOME/nix-config/scripts/toggle-theme.sh";
+      theme = "$HOME/nix-config/scripts/switch-theme.sh";
     };
 #    initExtra = ''
 #      export EDITOR=nano
