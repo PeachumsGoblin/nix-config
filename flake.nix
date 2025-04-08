789768353257ad5820e64ccd964cff550eb4aa4b
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-07 21:04:50 -0700

    Working in Stylix

--- flake.nix
+++ flake.nix
@@ -18,33 +18,24 @@
           inherit inputs;
           self = inputs.self;
         };
+        
+      modules = [
+        inputs.stylix.nixosModules.stylix
+        ./hosts/peach-koopy/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+          home-manager.extraSpecialArgs = {
+            inherit inputs;
+            self = inputs.self;
+            allowFileClobbering = true;
+          };
 
-
-modules = [
-  inputs.stylix.nixosModules.stylix
-  ./hosts/peach-koopy/configuration.nix
-  home-manager.nixosModules.home-manager
-  {
-    home-manager.useGlobalPkgs = true;
-    home-manager.useUserPackages = true;
-    home-manager.backupFileExtension = "backup";
-    home-manager.extraSpecialArgs = {
-      inherit inputs;
-      self = inputs.self;
-      allowFileClobbering = true;
+          home-manager.users.peachie = import ./home/peachie/home.nix;
+        }
+      ];
     };
-
-    home-manager.users.peachie = import ./home/peachie/home.nix;
-  }
-];
-
-
-
-
-
-            };
-          }
-        ];
-      };
-    };
+  };
 }
