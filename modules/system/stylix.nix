070593371f7c5f5b62e1820a4657193b2a96d2a8
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-07 18:20:11 -0700

    Working in Stylix

--- modules/system/stylix.nix
+++ modules/system/stylix.nix
@@ -3,51 +3,21 @@
 {
   stylix = {
     enable = true;
-
-    # Use a symlink for dynamic switching — points to one of your theme YAML files
     base16Scheme = ../../themes/current.yaml;
+    targets.hyprland.enable = true;
+    targets.kitty.enable = true;
+    targets.starship.enable = true;
+    targets.zsh.enable = true;
+#    kde.enable = true;
+    gtk.enable = true;
 
-    # Fonts
-    fonts = {
-      monospace = {
-        package = pkgs.monaspace;
-        name = "Monaspace Neon";
-      };
-      sansSerif = {
-        package = pkgs.noto-fonts;
-        name = "Noto Sans";
-      };
-      serif = {
-        package = pkgs.noto-fonts;
-        name = "Noto Serif";
-      };
-      emoji = {
-        package = pkgs.noto-fonts-emoji;
-        name = "Noto Color Emoji";
-      };
+    fonts.monospace = {
+      package = pkgs.monaspace;
+      name = "Monaspace Neon";
     };
-
-    # Cursor theme
     cursor = {
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Ice";
-      size = 24;
-    };
-
-    # Target applications/desktops to apply theme to
-    targets = {
-      gtk.enable = true;
- #    kde.enable = true;
-      hyprland.enable = true;
-      kitty.enable = true;
-      starship.enable = true;
-      fish.enable = false;
-      zsh.enable = true;
-      gnome.enable = false;
-    };
-
-    opacity = {
-      terminal = 0.90;
     };
   };
 }
