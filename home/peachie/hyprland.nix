827d0ebde2e043b8c767e5174463f4d639cfecff
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-07 23:12:27 -0700

    Hyprland edits

--- home/peachie/hyprland.nix
+++ home/peachie/hyprland.nix
@@ -109,7 +109,6 @@
     # WINDOW RULES
 
       windowrule = {
-        "suppressevent maximize, class:.*"
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
       };
     };
