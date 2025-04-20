526213e36c30d4ab7625825c060123a5f0850a1b
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-20 03:46:17 -0700

    working on Nvidia with Docker

--- modules/system/docker.nix
+++ modules/system/docker.nix
@@ -2,6 +2,7 @@
 
 {
   virtualisation.docker.enable = true;
+  virtualisation.docker.enableNvidia = true;
   environment.systemPackages = with pkgs; [
     docker
   ];
--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -6,6 +6,8 @@ in {
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia-container-toolkit.enable = true;
+  hardware.nvidia-container.enable = true;
+  hardware.opengl.enable = true;
 
   hardware.nvidia = {
     modesetting.enable = true;
