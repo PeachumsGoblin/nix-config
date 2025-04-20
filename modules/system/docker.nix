6ee5b20887b634cb973e87e82c7b9325a6f0f763
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-20 03:49:56 -0700

    working on Nvidia with Docker

--- modules/system/docker.nix
+++ modules/system/docker.nix
@@ -2,10 +2,8 @@
 
 {
   virtualisation.docker.enable = true;
-  virtualisation.docker.enableNvidia = true;
   environment.systemPackages = with pkgs; [
     docker
     nvidia-docker
-    nvidia-docker-toolkit
   ];
 }
