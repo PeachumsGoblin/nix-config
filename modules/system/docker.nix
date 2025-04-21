65b1d82de752509d80fa2834b9d4978d82a0f135
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-20 19:56:17 -0700

    working on Nvidia with Docker

--- modules/system/docker.nix
+++ modules/system/docker.nix
@@ -12,10 +12,12 @@
       "nvidia" = {
         "path" = "/usr/bin/nvidia-container-runtime";
         "runtimeArgs" = [];
+      };
     };
-  };
 
   # DO NOT set this unless you're sure the runtime exists
   # Leave this out for now:
   # virtualisation.docker.extraOptions = "--default-runtime=nvidia";
+    };
+  };
 }
