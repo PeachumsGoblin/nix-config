26176817cabe4b08865f9b197a85f9c5ad0a5d10
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-22 06:33:29 -0700

    Installing python

--- modules/system/docker.nix
+++ modules/system/docker.nix
@@ -3,18 +3,19 @@
 {
   virtualisation.docker = {
     enable = true;
-    nvidiaSupport = true;  # optional but harmless
-    extraConfig = lib.mkForce ''
-      {
-        "default-runtime": "nvidia",
-        "runtimes": {
-          "nvidia": {
-            "path": "${pkgs.nvidia-container-toolkit}/bin/nvidia-container-runtime",
-            "runtimeArgs": []
-          }
-        }
-      }
-    '';
+    package        = pkgs.docker_25;
+    nvidiaSupport  = true;
+    daemonConfig   = {
+# This JSON will be merged into /etc/docker/daemon.json
+      "default-runtime" = "nvidia";
+      "runtimes" = {
+        nvidia = {
+          path = "${pkgs.nvidia-container-toolkit}/bin/nvidia-container-runtime";
+          runtimeArgs = [];
+        };
+      };
+      };
+
   };
 
   environment.systemPackages = with pkgs; [
