303bce3c767bfa0a58ad017588d83bf086133277
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-22 06:34:32 -0700

    Installing python

--- modules/system/docker.nix
+++ modules/system/docker.nix
@@ -1,24 +1,24 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, ... }:
 
 {
-  services.docker = {
+  virtualisation.docker = {
     enable = true;
-    package        = pkgs.docker_25;
-    nvidiaSupport  = true;
-    daemonConfig   = {
-# This JSON will be merged into /etc/docker/daemon.json
-      "default-runtime" = "nvidia";
-      "runtimes" = {
-        nvidia = {
-          path = "${pkgs.nvidia-container-toolkit}/bin/nvidia-container-runtime";
-          runtimeArgs = [];
-        };
+    daemon.settings = {
+    "default-runtime" = "nvidia";
+    "runtimes" = {
+      "nvidia" = {
+        "path" = "${pkgs.nvidia-container-toolkit}/bin/nvidia-container-runtime";
+        "runtimeArgs" = [];
       };
-      };
+    };
 
+  # DO NOT set this unless you're sure the runtime exists
+  # Leave this out for now:
+  # virtualisation.docker.extraOptions = "--default-runtime=nvidia";
+    };
   };
 
   environment.systemPackages = with pkgs; [
-    docker
+      docker
   ];
 }
