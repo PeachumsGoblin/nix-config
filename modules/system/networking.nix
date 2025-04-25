004c8fb05ca870a73cd7486b3c3e1505723be313
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-25 13:18:31 -0700

    API Key

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -13,7 +13,7 @@
   ];
 
 environment.sessionVariables = rec {
-  OPENAI_API_KEY=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;
+  OPENAI_API_KEY="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";
 };
 
 
