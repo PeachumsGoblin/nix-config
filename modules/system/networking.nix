83b91414ef8b247cb2c8dd313f117101f99f472f
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-25 13:17:58 -0700

    API Key

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -11,5 +11,11 @@
   nixpkgs.config.permittedInsecurePackages = [
     "openssl-1.1.1w"
   ];
+
+environment.sessionVariables = rec {
+  OPENAI_API_KEY=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;
+};
+
+
 }
 
