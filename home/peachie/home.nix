7ca66d95b9e582bdd35e723459fd772888c1a9c7
Peachie Joyce <peachumsgoblin@pm.me>
2025-04-25 18:04:25 -0700

    Stylix

--- home/peachie/home.nix
+++ home/peachie/home.nix
@@ -21,7 +21,6 @@
   ];
 
   home.username = "peachie";
-  home.homeDirectory = lib.mkDefault (builtins.getEnv "HOME");
   home.stateVersion = "23.11";
 
   home.activation.preCleanup = lib.hm.dag.entryBefore ["checkLinkTargets"] ''
@@ -30,11 +29,6 @@
 
   home.activation.cleanupGtkrc = lib.hm.dag.entryAfter ["writeBoundary"] ''
     rm -f ~/.gtkrc-2.0
-  '';
-
-  # Make a Games directory
-  home.activation.createGamesDir = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    mkdir -p ${config.home.homeDirectory}/Games
   '';
 }
 
